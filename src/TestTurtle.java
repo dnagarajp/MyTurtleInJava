9048dc762dc0ab4bf8e65441ca462fc28876c9fa
Deepa.Nagarajappa <deepa.nagarajappa@inria.fr>
2021-12-30 03:17:56 +0100

    10 Commit: Review comments -- in progress

--- src/TestTurtle.java
+++ src/TestTurtle.java
@@ -9,116 +9,256 @@ class TestTurtle {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		turtle = new Turtle(-2, -30, 0);
+		turtle = new Turtle(-9, -5, 0);
 
 	}
 
+	/**
+	 * test initialValues of X and Y based on constructor if Negative values it
+	 * resets the X and Y points to 0. if no requirement of moving the x y , default
+	 * should be 0
+	 */
 	@Test
-	void testGoToPosition() {
-
-		turtle.goToPosition(5, 6);
-		assertEquals(5, turtle.x);
-		assertEquals(6, turtle.y);
+	void testInitialValueOfXY() {
+		assertEquals(0, turtle.getXPosition());
+		assertEquals(0, turtle.getYPosition());
 	}
 
+	/**
+	 * test X and Y based on the MoveTo method With negative values as an argument,
+	 * should contain given values to x and y respectively
+	 */
 	@Test
-	void testInitialPosition() {
-
-		assertEquals(0, turtle.x);
-		assertEquals(0, turtle.y);
+	void testMoveToWithNegativeValues() {
+		turtle.moveTo(-4, -5);
+		assertEquals(-4, turtle.getXPosition());
+		assertEquals(-5, turtle.getYPosition());
 	}
 
+	/**
+	 * test X and Y based the method using MoveTo should set the X and Y as per the
+	 * new values
+	 */
 	@Test
-	void testMoveSouth() {
-		turtle.moveSouth(3);
-		assertEquals(0, turtle.x);
-		assertEquals(-3, turtle.y);
+	void testMoveToWithPositiveValues() {
+		turtle.moveTo(4, 5);
+		assertEquals(4, turtle.getXPosition());
+		assertEquals(5, turtle.getYPosition());
 	}
 
+	/**
+	 * tests the direction can be set from 0-359 positive values towards clockwise.
+	 * direction. should show exactly what the direction if its in positive Numbers
+	 */
 	@Test
-	void testMoveNorth() {
-		turtle.moveNorth(5);
-		assertEquals(0, turtle.x);
-		assertEquals(5, turtle.y);
+	void testSetDirectionClockwise() {
+		turtle.setDirection(12);
+		assertEquals(12, turtle.getDirection());
 	}
 
+	/**
+	 * tests the direction can be set from 0-359 negative values towards anti
+	 * clockwise. direction. should show exactly what the direction [calculated with
+	 * "360 - directionValue" if its in negative Numbers
+	 */
 	@Test
-	void testMoveEast() {
-		turtle.moveEast(4);
-		assertEquals(4, turtle.x);
-		assertEquals(0, turtle.y);
+	void testSetDirectionAntiClockwise() {
+		turtle.setDirection(-12); // values means go from antiClockWise direction. starts from 360
+		assertEquals(348, turtle.getDirection()); // direction pointing to 348 degree because 360-12 = 348.
 	}
 
+	/**
+	 * With direction is 0, should move towards angle 0 degree, where x will
+	 * increment and y is constant i.e, 0
+	 */
 	@Test
-	void testMoveWest() {
-		turtle.moveWest(2);
-		assertEquals(-2, turtle.x);
-		assertEquals(0, turtle.y);
+	void testMove() {
+		turtle.moveForward(10);
+		assertEquals(10, turtle.getXPosition());
+		assertEquals(0, turtle.getYPosition());
 	}
 
+	/**
+	 * test negative values so that as a value to move should traverse back on the
+	 * same line
+	 */
 	@Test
-	void testInitialldirection() {
-		assertEquals(0, turtle.direction);
-
+	void testMoveNegative() {
+		turtle.moveForward(10);
+		turtle.moveForward(-10);
+		assertEquals(0, turtle.getXPosition());
+		assertEquals(0, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 1st quadrant perpendicular to x axis and y axis move
+	 * 10 steps towards 90 degree, should show the values x=0 and y=10
+	 */
 	@Test
-	void testSetdirection() {
-		turtle.setdirection(35);
-		assertEquals(35, turtle.direction);
+	void testMoveForward90() {
+		turtle.setDirection(90);
+		turtle.moveForward(10); //
+		assertEquals(90, turtle.getDirection());
+		assertEquals(0, turtle.getXPosition());
+		assertEquals(10, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 1st quadrant (+ +) with angle 45 degree and move
+	 * forward and should show the x and y positions as 7, 7 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
 	@Test
-	void testMoveForward() {
-		turtle.setdirection(45);
+	void testMoveForward1stQuadrant() {
+		turtle.setDirection(45);
+		turtle.moveForward(10);
+		assertEquals(45, turtle.getDirection());
+		assertEquals(7, turtle.getXPosition());
+		assertEquals(7, turtle.getYPosition());
+	}
+
+	/**
+	 * turtle is facing towards 2nd quadrant (- +) with angle 110 degree and move
+	 * forward and should show the x and y positions as -3, 9 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
+	@Test
+	void testMoveForward2ndQuadrant() {
+		turtle.setDirection(110);
+		turtle.moveForward(10);
+		assertEquals(110, turtle.getDirection());
+		assertEquals(-3, turtle.getXPosition());
+		assertEquals(9, turtle.getYPosition());
+	}
+
+	/**
+	 * turtle is facing towards 3rd quadrant (- -) with angle 195 degree and move
+	 * forward and should show the x and y positions as -8, -2 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
+	@Test
+	void testMoveForward3rdQuadrant() {
+		turtle.setDirection(195);
+		turtle.moveForward(8);
+		assertEquals(195, turtle.getDirection());
+		assertEquals(-8, turtle.getXPosition());
+		assertEquals(-2, turtle.getYPosition());
+	}
+
+	/**
+	 * turtle is facing towards 4th quadrant (+ -) with angle 277 degree and move
+	 * forward and should show the x and y positions as 1, -5 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
+	@Test
+	void testMoveForward4thQuadrant() {
+		turtle.setDirection(277);
 		turtle.moveForward(5);
-		assertEquals(0, turtle.x);
-		assertEquals(5, turtle.y);
-
+		assertEquals(277, turtle.getDirection());
+		assertEquals(1, turtle.getXPosition());
+		assertEquals(-5, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 1st quadrant perpendicular to x axis and y axis move
+	 * 10 steps backward in the 90 degree angle, should show the values x=0 and
+	 * y=-10.
+	 */
 	@Test
-	void testMoveBackward() {
-		turtle.setdirection(90);
+	void testMoveBackward90() {
+		turtle.setDirection(90);
+		turtle.moveBackward(10);
+		assertEquals(90, turtle.getDirection());
+		assertEquals(0, turtle.getXPosition());
+		assertEquals(-10, turtle.getYPosition());
+	}
+
+	/**
+	 * turtle is facing towards 4th quadrant (+ -) with angle 315 degree and move
+	 * forward then backward to see the x and y values are still in 4th
+	 * quadrant.should show the x and y positions as 1, -5 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
+	@Test
+	void testMoveBackward4thQuadrantWithinQuadrant() {
+		turtle.setDirection(315);
+		turtle.moveForward(15);
 		turtle.moveBackward(5);
-		assertEquals(0, turtle.x);
-		assertEquals(-5, turtle.y);
+		assertEquals(315, turtle.getDirection());
+		assertEquals(7, turtle.getXPosition());
+		assertEquals(-7, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 4th quadrant (+ -) with angle 320 degree and move
+	 * backward to see the x and y values are crossing and coming to 2nd Quadrant (-
+	 * +) from 4th quadrant.should show the x and y positions as -14, 12 .calculated
+	 * based on trigonometric formula with the help of radius.
+	 */
 	@Test
-	void testMoveLeft() {
-		turtle.setdirection(180);
-		turtle.moveLeft(5);
-		assertEquals(0, turtle.x);
-		assertEquals(-5, turtle.y);
+	void testMoveBackward4thQuadrantWithoutQuadrant() {
+		turtle.setDirection(320);
+		turtle.moveBackward(18);
+		assertEquals(320, turtle.getDirection());
+		assertEquals(-14, turtle.getXPosition());
+		assertEquals(12, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 3rd quadrant (- -) with angle 250 degree and move
+	 * forward then backward to see the x and y values are still in 3rd
+	 * quadrant.should show the x and y positions as -2, -5 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
 	@Test
-	void testMoveRight() {
-		turtle.setdirection(270);
-		turtle.moveRight(5);
-		assertEquals(-5, turtle.x);
-		assertEquals(0, turtle.y);
+	void testMoveBackward3rdQuadrant() {
+		turtle.setDirection(250);
+		turtle.moveForward(15);
+		turtle.moveBackward(10);
+		assertEquals(250, turtle.getDirection());
+		assertEquals(-2, turtle.getXPosition());
+		assertEquals(-5, turtle.getYPosition());
 	}
 
+	/**
+	 * turtle is facing towards 2nd quadrant (- +) with angle 165 degree and move
+	 * forward then backward to see the x and y values are still in 2nd
+	 * quadrant.should show the x and y positions as -9, -3 .calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
 	@Test
-	void testLog() {
-		turtle.setdirection(270);
-		turtle.moveRight(5);
-		assertEquals(2, turtle.arrayLog.size());
-		assertEquals("setdirection() 270", turtle.arrayLog.get(0));
-		assertEquals("moveRight() 5", turtle.arrayLog.get(1));
+	void testMoveBackward2ndQuadrant() {
+		turtle.setDirection(165);
+		turtle.moveForward(18);
+		turtle.moveBackward(8);
+		assertEquals(165, turtle.getDirection());
+		assertEquals(-9, turtle.getXPosition());
+		assertEquals(3, turtle.getYPosition());
+	}
 
+	/**
+	 * turtle is facing towards 1st quadrant (+ +) with angle 83 degree and move
+	 * forward then backward to see the x and y values are still in 1st
+	 * quadrant.should show the x and y positions as 1, 15. calculated based on
+	 * trigonometric formula with the help of radius.
+	 */
+	@Test
+	void testMoveBackward1stQuadrant() {
+		turtle.setDirection(83);
+		turtle.moveForward(20);
+		turtle.moveBackward(5);
+		assertEquals(83, turtle.getDirection());
+		assertEquals(1, turtle.getXPosition());
+		assertEquals(15, turtle.getYPosition());
 	}
 
 	@Test
 	void testReplay() {
 
-		turtle.goToPosition(0, 0);
-		turtle.setdirection(90);
+		turtle.moveTo(0, 0);
+		turtle.setDirection(90);
 		turtle.moveForward(10);
-		turtle.replay(1, 2); // totally the size is 3 together commands here. 1 is reply and 1st - setdirection,
-								// 2nd forward()
+		turtle.replay(1, 2);
 		assertEquals(0, turtle.x);
 		assertEquals(20, turtle.y);
 		assertEquals(90, turtle.direction);
@@ -128,11 +268,11 @@ class TestTurtle {
 	@Test
 	void testReplayTwice() {
 
-		turtle.goToPosition(0, 0);
-		turtle.setdirection(90);
+		turtle.moveTo(0, 0);
+		turtle.setDirection(90);
 		turtle.moveForward(10);
-		turtle.replay(1, 2); // totally the size is 3 together commands here. 
-		turtle.replay(0, 3); // this contains 5 commands in total
+		turtle.replay(1, 2);
+		turtle.replay(0, 3);
 		assertEquals(0, turtle.x);
 		assertEquals(20, turtle.y);
 		assertEquals(90, turtle.direction);
@@ -143,8 +283,8 @@ class TestTurtle {
 	@Test
 	void testYalper() {
 
-		turtle.goToPosition(0, 0);
-		turtle.setdirection(90);
+		turtle.moveTo(0, 0);
+		turtle.setDirection(90);
 		turtle.moveForward(10);
 		turtle.yalper(2,1); 
 		assertEquals(0, turtle.x);
@@ -157,8 +297,8 @@ class TestTurtle {
 	@Test
 	void testYalperTwice() {
 
-		turtle.goToPosition(0, 0);
-		turtle.setdirection(90);
+		turtle.moveTo(0, 0);
+		turtle.setDirection(90);
 		turtle.moveForward(10);
 		turtle.yalper(2, 1); 
 		turtle.yalper(3, 1); 
@@ -166,7 +306,7 @@ class TestTurtle {
 		assertEquals(0, turtle.x);
 		assertEquals(30,turtle.y);
 		assertEquals(90,turtle.direction);
-		assertEquals(10, turtle.arrayLog.size());
+		assertEquals(9, turtle.arrayLog.size());
 	}
 	
 
@@ -177,9 +317,10 @@ class TestTurtle {
 
 	@Test
 	void testListContains() {
-		turtle.goToPosition(3, 8);
+		turtle.moveTo(3, 8);
 		turtle.getList();
 		assertEquals(1, turtle.arrayLog.size());
 	}
+
 
 }
--- src/Turtle.java
+++ src/Turtle.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class Turtle {
@@ -7,11 +6,19 @@ public class Turtle {
 
 	}
 
-	public int x;
-	public int y;
-	public int direction;
-	public ArrayList<String> arrayLog;
+	// declare variables
+	protected int x;
+	protected int y;
+	protected int direction;
+	protected ArrayList<String> arrayLog;
 
+	/**
+	 * Object Constructor initialization
+	 * 
+	 * @param x         position value
+	 * @param y         position value
+	 * @param direction or angle of the circle in degrees
+	 */
 	public Turtle(int x, int y, int direction) {
 		if (x < 0) {
 			x = 0;
@@ -26,113 +33,80 @@ public class Turtle {
 
 	}
 
-	public void moveSouth(int steps) {
-
-		y = y - steps;
-	}
-
-	public void moveNorth(int steps) {
-
-		y = y + steps;
-
-	}
-
-	public void moveEast(int steps) {
-
-		x = x + steps;
-
-	}
-
-	public void moveWest(int steps) {
-
-		x = x - steps;
-
-	}
-
-	public void setdirection(int directionValue) {
-
-		arrayLog.add("setdirection()" + " " + directionValue);
-
-		direction = directionValue;
-
-	}
-
-	public void moveForward(int steps) {
-		// only expects 0,90,180 and 270
-		arrayLog.add("moveForward()" + " " + steps);
-		if (direction == 0) {
-			this.moveEast(steps);
-		} else if (direction <= 90) {
-			this.moveNorth(steps);
-		} else if (direction == 180) {
-			this.moveWest(steps);
-		} else {
-			this.moveSouth(steps);
-		}
-	}
-
-	public void moveBackward(int steps) {
-		// only expects 0,90,180 and 270
-		arrayLog.add("moveBackward()" + " " + steps);
-		if (direction == 0) {
-			this.moveWest(steps);
-		} else if (direction == 90) {
-			this.moveSouth(steps);
-		} else if (direction == 180) {
-			this.moveEast(steps);
-		} else {
-			this.moveNorth(steps);
-		}
-	}
-
-	public void moveLeft(int steps) {
-		// only expects 0,90,180 and 270
-		arrayLog.add("moveLeft()" + " " + steps);
-		if (direction == 0) {
-			this.moveNorth(steps);
-		} else if (direction == 90) {
-			this.moveWest(steps);
-		} else if (direction == 180) {
-			this.moveSouth(steps);
-		} else {
-			this.moveEast(steps);
-		}
-	}
-
-	public void moveRight(int steps) {
-		// only expects 0,90,180 and 270
-		arrayLog.add("moveRight()" + " " + steps);
-		if (direction == 0) {
-			this.moveSouth(steps);
-		} else if (direction == 90) {
-			this.moveEast(steps);
-		} else if (direction == 180) {
-			this.moveNorth(steps);
-		} else {
-			this.moveWest(steps);
-		}
-	}
-
-	public void goToPosition(int xValue, int yValue) {
-
-		arrayLog.add("goToPosition()" + " " + xValue + " " + yValue);
-		x = xValue;
-		y = yValue;
-	}
-	
+	// return the current x position
 	public int getXPosition() {
 		return this.x;
 
 	}
-	
+
+	// return the current y position
 	public int getYPosition() {
 		return this.y;
 
 	}
-	
+
+	// return the current direction or angle
 	public int getDirection() {
-	    return this.direction;
-	  }
+		return this.direction;
+	}
+
+	/**
+	 * set the direction in [0-359]
+	 * 
+	 * @param direction positive numbers goes with clockwise direction, Negative
+	 *                  numbers goes with backward direction
+	 */
+	public void setDirection(int direction) {
+		arrayLog.add("setDirection()" + " " + direction);
+		direction = (direction % 360);
+		if (direction < 0) {
+			direction = direction + 360;
+		}
+
+		this.direction = direction;
+	}
+
+	public void moveTo(int x, int y) {
+		arrayLog.add("moveTo()" + " " + x + " " + y);
+		this.x = x;
+		this.y = y;
+
+	}
+
+	/**
+	 * Makes a turtle move forward with respect to its current angle
+	 * 
+	 * @param amount The distance for the turtle to move. This number is taken and
+	 *               conversion from degree to radius is stored and used to
+	 *               calculate the x and y positions by adding the old x and y
+	 *               values.
+	 */
+	public void moveForward(int step) {
+		arrayLog.add("moveForward()" + " " + step);
+		double dirRads = Math.PI * (this.direction / 180.0);
+		int xStep = x + (int) Math.round(Math.cos(dirRads) * step);
+		int yStep = y + (int) Math.round(Math.sin(dirRads) * step);
+		this.x = xStep;
+		this.y = yStep;
+	}
+
+	/**
+	 * Makes a turtle move backward with respect to its current angle
+	 * 
+	 * @param amount The distance for the turtle to move. This number is taken and
+	 *               conversion from degree to radius is stored and used to
+	 *               calculate the x and y positions by subtracting the old x and
+	 *               old y values.
+	 */
+	public void moveBackward(int step) {
+		arrayLog.add("moveBackward()" + " " + step);
+		double dirRads = Math.PI * (this.direction / 180.0);
+		int endXStep = x - (int) Math.round(Math.cos(dirRads) * step);
+		int endYStep = y - (int) Math.round(Math.sin(dirRads) * step);
+		this.x = endXStep;
+		this.y = endYStep;
+
+	}
 
 	public void replay(int startNumber, int endNumber) {
 
@@ -146,30 +120,25 @@ public class Turtle {
 				int attribute = Integer.parseInt(attributeValue);
 
 				switch (methodName) {
+
+				case "setDirection()":
+					this.setDirection(attribute);
+					break;
+				case "moveTo()":
+					String getPosition2 = parts[2];
+					int position2 = Integer.parseInt(getPosition2);
+					this.moveTo(attribute, position2);
+					break;
 				case "moveForward()":
 					this.moveForward(attribute);
 					break;
 				case "moveBackward()":
 					this.moveBackward(attribute);
 					break;
-				case "moveLeft()":
-					this.moveLeft(attribute);
-					break;
-				case "moveRight()":
-					this.moveRight(attribute);
-					break;
-				case "setdirection()":
-					this.setdirection(attribute);
-					break;
 				case "replay()":
 					String attributeValue2 = parts[2];
 					int attribute2 = Integer.parseInt(attributeValue2);
 					this.replay(attribute, attribute2);
-					break;
-				case "goToPosition()":
-					String getPosition2 = parts[2];
-					int position2 = Integer.parseInt(getPosition2);
-					this.goToPosition(attribute, position2);
 					break;
 				default:
 					System.out.println("Invalid method name");
@@ -200,45 +169,33 @@ public class Turtle {
 				int attribute = Integer.parseInt(attributeValue);
 
 				switch (methodName) {
+
+				case "setDirection()":
+					this.setDirection(attribute);
+					break;
+				case "moveTo()":
+					String getPosition2 = parts[2];
+					int position2 = Integer.parseInt(getPosition2);
+					this.moveTo(attribute, position2);
+					break;
 				case "moveForward()":
 					this.moveForward(attribute);
 					break;
 				case "moveBackward()":
 					this.moveBackward(attribute);
 					break;
-				case "moveLeft()":
-					this.moveLeft(attribute);
-					break;
-				case "moveRight()":
-					this.moveRight(attribute);
-					break;
-				case "setdirection()":
-					this.setdirection(attribute);
-					break;
 				case "replay()":
 					String attributeValue2 = parts[2];
 					int attribute2 = Integer.parseInt(attributeValue2);
 					this.replay(attribute, attribute2);
 					break;
-				case "goToPosition()":
-					String getPosition2 = parts[2];
-					int position2 = Integer.parseInt(getPosition2);
-					this.goToPosition(attribute, position2);
-					break;
-
-				case "yalper()":
-					String positionValue = parts[2];
-					int attributeYalper2 = Integer.parseInt(positionValue);
-					this.replay(attribute, attributeYalper2);
-					break;
-
 				default:
 					System.out.println("Invalid method name");
+
 				}
 			}
 		} else {
 			System.out.println("End Number is greater than the arrayLog size");
 		}
-
 	}
 }
